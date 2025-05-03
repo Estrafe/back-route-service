db41372030ac8e50739f486165395687bb5670ba
diegxherrera <diegoherred@gmail.com>
2025-05-03 12:12:19 +0200

    Initial commit

---
+++ src/main/java/me/diegxherrera/backrouteservice/BackRouteServiceApplication.java
@@ -0,0 +1,13 @@
+package me.diegxherrera.backrouteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackRouteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackRouteServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/diegxherrera/backrouteservice/BackRouteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.diegxherrera.backrouteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRouteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
